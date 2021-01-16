2000d691b31764fe76df818cfd8ea12ff9d14283
Lê Văn Kỳ Thịnh <kythinhle@gmail.com>
2021-01-16 15:44:06 +0700

    Release version 1.1.10

--- wp-content/plugins/woocommerce-gateway-pace/woocommerce-gateway-pace.php
+++ wp-content/plugins/woocommerce-gateway-pace/woocommerce-gateway-pace.php
@@ -5,7 +5,7 @@
  * Description: Provides Pace as a payment method in WooCommerce.
  * Author: Pace Enterprise Pte Ltd
  * Author URI: https://developers.pacenow.co/#plugins-woocommerce
- * Version: 1.1.9
+ * Version: 1.1.10
  * Requires at least: 5.3
  * WC requires at least: 3.0
  * Requires PHP: 7.*
@@ -20,7 +20,7 @@ if (!defined('ABSPATH')) {
 /**
  * Required minimums and constants
  */
-define('WC_PACE_GATEWAY_VERSION', '1.1.9');
+define('WC_PACE_GATEWAY_VERSION', '1.1.10');
 define('WC_PACE_GATEWAY_NAME', 'Pace For WooCommerce');
 define('WC_PACE_GATEWAY_MIN_WC_VER', '3.0');
 define('WC_PACE_MAIN_FILE', __FILE__);
